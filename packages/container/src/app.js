b3db029b66cdc07980273297c625ef9f2694d8a5
Mohit Singh <mohitsingh8@deloitte.com>
2024-01-27 19:59:10 +0530

    initial commit

---
+++ packages/container/src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MarketingComponent from "../components/marketing.component";
+
+export default () => {
+  return (
+    <div>
+      <h1>Hi there!</h1>
+      <hr />
+      <MarketingComponent />
+    </div>
+  );
+};
